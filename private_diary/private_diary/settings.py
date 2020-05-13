4fbfe1bd493d79a569d2faa204124b4386a8f4c0
snova <awaawapurin@gmail.com>
2020-05-13 15:43:37 +0900

    updated

--- private_diary/private_diary/settings.py
+++ private_diary/private_diary/settings.py
@@ -124,42 +124,47 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # ロギング設定
 LOGGING = {
-    'version': 1,
+    'version': 1,  # 1固定
     'disable_existing_loggers': False,
 
+    # ロガーの設定
     'loggers': {
+        # Djangoが利用するロガー
         'django': {
             'handlers': ['console'],
             'level': 'INFO',
         },
+        # diaryアプリケーションが利用するロガー
         'diary': {
             'handlers': ['console'],
             'level': 'DEBUG',
         },
     },
 
+    # ハンドラの設定
     'handlers': {
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
-            'formatter': 'dev',
+            'formatter': 'dev'
         },
     },
 
+    # フォーマッタの設定
     'formatters': {
         'dev': {
             'format': '\t'.join([
                 '%(asctime)s',
                 '[%(levelname)s]',
                 '%(pathname)s(Line:%(lineno)d)',
-                '%(message)s',
+                '%(message)s'
             ])
         },
     }
 }
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
